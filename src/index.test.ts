f0fc9a5e9b177fe57aad1f25b75a32447f06e004
Michał Warda <michal.warda@elpassion.pl>
2022-03-22 11:17:04 +0100

    Implement division without edge cases

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,6 +9,10 @@ class TurboNumber {
   result(): number {
     return this.number;
   }
+
+  divide(number: number) {
+    this.number = this.number / number;
+  }
 }
 
 it("subtract 2 from 5", () => {
@@ -21,4 +25,10 @@ it("subtract 3 from 8", () => {
   const turboNumber = new TurboNumber(8);
   turboNumber.subtract(3)
   expect(turboNumber.result()).toBe(5)
+})
+
+it("divides 8 by 2", () => {
+  const turboNumber = new TurboNumber(8);
+  turboNumber.divide(2)
+  expect(turboNumber.result()).toBe(4)
 })
