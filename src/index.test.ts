3d3568c24922d701425f9dd6cbf9195f74fe150b
Michał Warda <michal.warda@elpassion.pl>
2022-03-22 11:22:52 +0100

    Introduce test setup

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,22 +1,26 @@
 import {TurboNumber} from "./TurboNumber";
 
-it("subtract 2 from 5", () => {
-  const turboNumber = new TurboNumber(5);
-  turboNumber.subtract(2)
-  expect(turboNumber.result()).toBe(3)
-})
+describe('TurboNumber', () => {
+  let turboNumber: TurboNumber;
 
-it("subtract 3 from 8", () => {
-  const turboNumber = new TurboNumber(8).subtract(3);
-  expect(turboNumber.result()).toBe(5)
-})
+  beforeEach(() => {
+    turboNumber = new TurboNumber(8);
+  })
+  it("subtract 2 from 5", () => {
+    turboNumber.subtract(2)
+    expect(turboNumber.result()).toBe(6)
+  })
 
-it("divides 8 by 2", () => {
-  const turboNumber = new TurboNumber(8).divide(2)
-  expect(turboNumber.result()).toBe(4)
-})
+  it("subtract 3 from 8", () => {
+    expect(turboNumber.subtract(3).result()).toBe(5)
+  })
 
-it("throws when dividing by 0", () => {
-  const turboNumber = new TurboNumber(8);
-  expect(() => turboNumber.divide(0)).toThrow('Cannot divide by 0')
+  it("divides 8 by 2", () => {
+    expect(turboNumber.divide(2).result()).toBe(4)
+  })
+
+  it("throws when dividing by 0", () => {
+    const turboNumber = new TurboNumber(8);
+    expect(() => turboNumber.divide(0)).toThrow('Cannot divide by 0')
+  })
 })
